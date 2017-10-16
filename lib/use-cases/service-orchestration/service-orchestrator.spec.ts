6911e104973bfbc94803cd7dd4cb79a168123144
Devon Humes <devonh@qualtrics.com>
2017-10-15 23:42:41 -0400

    initial commit

---
+++ lib/use-cases/service-orchestration/service-orchestrator.spec.ts
@@ -0,0 +1,16 @@
+
+import { ServiceOrchestratorFixture } from './service-orchestrator.fixture';
+
+describe('Service Orchestrator', () => {
+
+    let fixture: ServiceOrchestratorFixture;
+
+    beforeEach(() => {
+        fixture = new ServiceOrchestratorFixture();
+    });
+
+    it('test 1', function() {
+
+    });
+
+});
---
+++ wallaby.js
@@ -0,0 +1,19 @@
+module.exports = function () {
+    return {
+        files: [
+            'lib/use-cases/service-orchestration/service-orchestrator.ts',
+        ],
+
+        tests: [
+            'lib/use-cases/service-orchestration/service-orchestrator.spec.ts',
+            'lib/use-cases/service-orchestration/service-orchestrator.fixture.ts'
+        ],
+        env: {
+            type: 'node'
+        },
+        testFramework: 'mocha',
+        workers: {
+            recycle: true
+        }
+    };
+};
